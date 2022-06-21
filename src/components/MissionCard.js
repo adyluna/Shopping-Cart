616cf227b5390ec9a94e01ab6d2db7d32cff78b1
adyluna <adylunaleite@gmail.com>
2022-06-21 18:21:45 -0300

    lint ok

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -6,7 +6,7 @@ class MissionCard extends React.Component {
     const { name, year, country, destination } = this.props;
 
     return (
-      <div className='mission' data-testid="mission-card">
+      <div className="mission" data-testid="mission-card">
         <p data-testid="mission-name">{name}</p>
         <p data-testid="mission-year">{year}</p>
         <p data-testid="mission-country">{country}</p>
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -6,7 +6,7 @@ import PlanetCard from './PlanetCard';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div className='solarSystem' data-testid="solar-system">
+      <div className="solarSystem" data-testid="solar-system">
         <Title headline="Planetas" />
         {Planets.map(({
           name,
