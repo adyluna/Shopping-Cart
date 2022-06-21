1aea57cbb38dbc24b64e1da0ea926fb3e5171c1b
adyluna <adylunaleite@gmail.com>
2022-06-21 14:49:13 -0300

    1 and 2 ok

--- src/SolarSystem.js
+++ src/SolarSystem.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 class SolarSystem extends React.Component {
-    render() {
-        return (
-            <div data-testid="solar-system">
-                <p>Testing</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>Testing</p>
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
